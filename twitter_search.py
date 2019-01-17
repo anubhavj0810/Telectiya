f8358dce1bdec4744a23c0e366953fe15bf0bb6d
anubhavj0810 <anubhavj0810@gmail.com>
2019-01-18 00:33:41 +0530

    Update twitter_search.py
    Updated twitter search code for historic tweets

--- twitter_search.py
+++ twitter_search.py
@@ -4,34 +4,41 @@ import pandas as pd
 import time
 
 
+import twitter
 from TwitterSearch import *
 
-try:
-    tuo = TwitterUserOrder('@BJP4India') # create a TwitterUserOrder
 
-    tso = TwitterSearchOrder()
+def twitter_search_tweets(string,abb):
+    try:
+        tuo = TwitterUserOrder('@BJP4India') # create a TwitterUserOrder
 
-    tso.set_keywords(['BJP', 'INC','AAP'])  # let's define all words we would like to have a look for
-    tso.set_language(lang='en')
-    tso.set_include_entities(False)
+        tso = TwitterSearchOrder()
 
-    # it's about time to create TwitterSearch object again
-    ts = TwitterSearch(consumer_key = CONSUMER_KEY,consumer_secret = CONSUMER_SECRET,access_token = ACCESS_KEY,access_token_secret = ACCESS_SECRET)
+        tso.set_keywords([string,abb])  # let's define all words we would like to have a look for
+        tso.set_language(lang='en')
+        tso.set_include_entities(False)
 
-    for tweet in ts.search_tweets_iterable(tso):
-        print('@%s tweeted: %s' % (tweet['user']['screen_name'], tweet['text']))
+        # it's about time to create TwitterSearch object again
+        ts = TwitterSearch(consumer_key = CONSUMER_KEY,consumer_secret = CONSUMER_SECRET,access_token = ACCESS_KEY,access_token_secret = ACCESS_SECRET)
 
-    # start asking Twitter about the timeline
+        for tweet in ts.search_tweets_iterable(tso):
+            print('@%s tweeted: %s' % (tweet['user']['screen_name'].encode('ascii', 'ignore').decode('ascii'), tweet['text'].encode('ascii', 'ignore').decode('ascii')))
 
-    """t_before = time.time()
-    count=0
-    for tweet in ts.search_tweets_iterable(tuo):
-        count+=1
-        print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
-    print(count)
-    t_after = time.time()
+        # start asking Twitter about the timeline
 
-    print("Time taken is ...")
-    print(t_after - t_before)"""
-except TwitterSearchException as e: # catch all those ugly errors
-    print(e)
+        """t_before = time.time()
+        count=0
+        for tweet in ts.search_tweets_iterable(tuo):
+            count+=1
+            print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+        print(count)
+        t_after = time.time()
+
+        print("Time taken is ...")
+        print(t_after - t_before)"""
+    except TwitterSearchException as e: # catch all those ugly errors
+        print(e)
+
+print("Enter the party name and its abbreviation")
+string,abb = input().split()
+twitter_search_tweets(string,abb)
