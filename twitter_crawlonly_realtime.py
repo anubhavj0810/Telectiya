82afd4c334f20191407b5f17f740dc47371be70b
anubhavj0810 <anubhavj0810@gmail.com>
2019-01-18 00:27:06 +0530

    Update twitter_crawlonly_realtime.py
    Crawling file updated by removing requests error

--- twitter_crawlonly_realtime.py
+++ twitter_crawlonly_realtime.py
@@ -2,16 +2,27 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from urllib3.exceptions import ProtocolError,ReadTimeoutError
 
+import json
 
 #Variables that contains the user credentials to access Twitter API
 
+politics = ["2019 elections", "Modi" ,"Narendra Modi ", "PMO India ", "Arvind Kejriwal ", "Arun Jaitley ", "Amit Shah ", "Sushma Swaraj ", "Rajnath Singh ", "BJP ","Bhartiya Janata Party","Bhartiya Janta Party ", "Akhilesh Yadav ", "Smriti Z Irani ", "Rahul Gandhi ", "Subramanian Swamy ", "Shashi Tharoor ", "Manohar Parrikar ", "ShivrajSingh Chouhan ", "Piyush Goyal ", "AAP ","Aam Aadmi Party", "Indian National Congress ", "Lalu Prasad Yadav ", "Nitin Gadkari ", "N Chandrababu Naidu ", "Vasundhara Raje ", "Devendra Fadnavis ", "Yogi Adityanath ", "Omar Abdullah ", "Ravi Shankar Prasad ", "HMO India ", "CM Office, GoUP ", "Nandan Nilekani ", "Vijay Rupani ", "Dr Raman Singh ", "Kapil Sibal ", "Nirmala Sitharaman ", "Raveesh Kumar ", "Kalam Center ", "Aaditya Thackeray ", "Sushil Kumar Modi ", "Manish Sisodia ", "Dr. Harsh Vardhan ", "Milind Deora ", "Samajwadi Party ", "Shahnawaz Hussain ", "Vijay Kumar Singh ", "Prakash Javadekar ", "Tejashwi Yadav "]
+choices1 = ['bjp', 'aap', 'bhajpaa', 'bhartiya janata party', 'aam aadmi party',
+           'bahujan samaj party', 'Samajwadi Party', 'AIADMK', 'DMK', 'Trinamool Congress', 'Shiv Sena',
+           'Nationalist Congress Party', 'Biju Janata Dal', 'Janata Dal United', 'JDU', 'Rastriya Janata Dal', 'RJD',
+           'JDS', 'Communist Party of India', 'Communist Party of India- Marxist', 'CPI-M', 'CPI']
+politics.extend(choices1)
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
         f.write(data)
+        tweet=json.loads(data)
+        #print(tweet['text'])
+
         return True
 
     def on_error(self, status):
@@ -26,10 +37,11 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    f=open('/Users/anubhavjain/Desktop/twitter_data.txt','a+')
+    f=open('/Users/anubhavjain/Desktop/twitter_data_1.txt','a+')
     #This line filter Twitter Streams to capture data by the given keywords:
-    stream.filter(track=['bjp', 'indian national congress', 'modi','rahul gandhi','inc','bhartiya janta party'])
-
-    #print("abcd")
-    f.close()
-
+    #stream.filter(follow=["207809313"])
+    while (True):
+        try:
+            stream.filter(track=politics, locations=[68.1766451354, 7.96553477623, 97.4025614766, 35.4940095078],languages=['en', 'hi', 'tl'], filter_level='low')
+        except (ProtocolError,AttributeError,ReadTimeoutError):
+            continue;
